d0402862fecb39a8c41ee6a4e79f1c799a7b78d9
Kat Hoffert <kat.hoffert@gmail.com>
2016-10-18 22:27:45 -0600

    Mailing working

--- mail.php
+++ mail.php
@@ -1,49 +1,69 @@
 <?php
+	// From http://php.net/manual/en/function.mail.php#108669
+	function mailUtf8($to, $from_user, $from_email, $subject, $message) {
+			$from_user = "=?UTF-8?B?".base64_encode($from_user)."?=";
+			$subject = "=?UTF-8?B?".base64_encode($subject)."?=";
 
-$name =""; // Sender Name
-$email =""; // Sender's email ID
-$phone =""; //Sender's Phone Number
-$message =""; // Sender's Message
+			$headers = "From: $from_user <$from_email>\r\n" .
+					"MIME-Version: 1.0" . "\r\n" .
+					"Content-type: text/html; charset=UTF-8" . "\r\n";
 
-$nameError ="";
-$emailError ="";
-$phoneError ="";
-$messageError ="";
-$errors = 0;
-$successMessage ="";
+		return mail($to, $subject, $message, $headers);
+	}
 
-$formcontent=" From: $name \n Email: $email \n Phone: $phone \n Message: $message";
-$recipient = "info@computerswithpride.ca";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-
-if(isset($_POST['submit'])) { // Checking null values in message.
-
-	$name = $_POST["name"]; // Sender Name
-	$email = $_POST["email"]; // Sender's email ID
-	$phone = $_POST["phoneNumber"];  //Sender's Phone Number
-	$message = $_POST["message"]; // Sender's Message
+	// Super simple validation
+	$isValid = true;
+	$name = $_REQUEST['name'];
+	$email = $_REQUEST['email'];
+	$phone = $_REQUEST['phone'];
+	$message = $_REQUEST['message'];
 	
-	if (empty($_POST["name"])){
-		$nameError = "Name is required";
-		$errors = 1;
+	if (trim($name) === '') {
+		$isValid = false;
 	}
-	if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL))
-	{
-		$emailError = "Email is not valid";
-		$errors = 1;
+	if (trim($email) === '') {
+		$isValid = false;
 	}
-	if (empty($_POST["phone"])){
-		$phoneError = "Phone number is required";
-		$errors = 1;
+	// This is actually a bit stricter than the client side validation, but it shouldn't be an issue
+	// since valid emails *shouldn't* be failing this...
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		$isValid = false;
 	}
-	if (empty($_POST["message"])){
-		$messageError = "Message is required";
-		$errors = 1;
+	if (trim($phone) === '') {
+		$isValid = false;
 	}
-}
+	if (trim($message) === '') {
+		$isValid = false;
+	}
 
-if($errors == 0){
-	$successMessage ="Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>"; // IF no errors
-}
+	if($isValid) {
+		// Replace newlines with HTML line breaks for the message
+		$message = str_replace("\n", "<br />", $message);
+
+		$emailBody = "From: $name<br/>Email: $email<br />Phone: $phone<br />Message: $message";
+		$success = mailUtf8("info@computerswithpride.ca", "Computer with Pride contact form",
+				"no-reply@computerswithpride", "Contact Form", $emailBody);
+
+		// Debug: Just print what would have been sent (note nothing printed for invalid form)
+		//echo $emailBody;
+
+		// Something is wrong with the mailer -- critical failure
+		// Consider just setting `$isValid` to false here instead of the current body, so that
+		// failures will send folks to the index page with just a generic message.
+		if(!$success) {
+			echo "<h1>HTTP 500 error</h1>";
+			echo "<p>Something went wrong. This shouldn't have happened. Please try again later or give us a call instead.</p>";
+			echo "Details: " . error_get_last()["message"];
+			die();
+		}
+	}
+
+	// Redirect user back to the main page
+	if($isValid) {
+		header("Location: index.html?success=true");
+	}
+	else {
+		header("Location: index.html?success=false");
+	}
+	exit();
 ?>
